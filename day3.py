90a2990bf3744275877c03c74b43c75977794b30
Saketh Katta <sakethkatta@gmail.com>
2025-12-02 16:18:26 -0500

    Day 3 Solutions

---
+++ day3.py
@@ -0,0 +1,26 @@
+file = open("input3.txt")
+lines = [line.strip() for line in file.readlines()]
+
+result = 0
+for line in lines:
+    joltage = 0
+    for i in range(len(line)):
+        for j in range(i + 1, len(line)):
+            joltage = max(joltage, int(line[i] + line[j]))
+    result += joltage
+print(result)
+
+result = 0
+for line in lines:
+    start = 0
+    batteries = []
+    turnOn = 12
+    while turnOn > 0:
+        end = len(line) - turnOn + 1
+        maxBattery = max(line[start:end])
+        batteryIndex = line.index(maxBattery, start, end)
+        start = batteryIndex + 1
+        batteries.append(maxBattery)
+        turnOn -= 1
+    result += int("".join(batteries))
+print(result)
